71e5e25371ad76d7a106394af73370c38f3ce3b5
Phil Peterson <phpeterson@usfca.edu>
2024-02-01 10:48:06 -0800

    fix end-of-lecture bug

--- week02/section01/even/even_s.s
+++ week02/section01/even/even_s.s
@@ -3,10 +3,10 @@
 # a0 contains a number we want to know is even or odd
 
 even_s:
-    li t0, 1            # default even = true
-    and t0, a0, t0      # bitwise AND with 1
+    li t1, 1            # t1 holds ret val, default true
+    andi t0, a0, 1      # bitwise AND with 1
     beq t0, zero, even  # if AND result is zero, it's even
-    li t0, 1            # a0 was odd
+    li t1, 0            # a0 was odd so ret val = false
 even:
-    mv a0, t0           # set up return value
-    ret
+    mv a0, t1           # set up return value
+    ret                 # return to caller
