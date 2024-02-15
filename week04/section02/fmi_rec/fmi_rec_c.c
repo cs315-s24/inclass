9237b4db856ad638a06068a2f73019ca96572703
Phil Peterson <phpeterson@usfca.edu>
2024-02-15 14:29:15 -0800

    add s02 2/15

--- week04/section02/fmi_rec/fmi_rec_c.c
+++ week04/section02/fmi_rec/fmi_rec_c.c
@@ -1,13 +1,24 @@
 int fmi_rec_c(int *arr, int len, int idx, int max_idx) {
     int max_new = max_idx;
 
+    // base case, cond branch to done?
     if (idx >= len) {
         return max_idx;
     }
 
+    /*
+        calc offset of idx'th elem and max'th elem
+        load the into regs so we can compare
+        new max, cond branch to skip over it
+    */
     if (arr[idx] > arr[max_idx]) {
         max_new = idx;
     }
 
+    /*
+        set up func args for recursion: a2+1
+        calling a function, save ra
+        nothing to do when recursion returns, a0 already ret val
+    */
     return fmi_rec_c(arr, len, idx + 1, max_new);
 }
--- week04/section02/fmi_rec/fmi_rec_s.s
+++ week04/section02/fmi_rec/fmi_rec_s.s
@@ -1,5 +1,30 @@
 .global fmi_rec_s
 
 fmi_rec_s:
-    mv a0, zero
+    addi sp, sp, -8         # prologue
+    sd ra, (sp)             # preserve ra
+
+    mv t0, a3               # max_new = max_idx
+    bge a2, a1, done        # if (idx >= len)
+
+    li t2, 4                # elems are 4 bytes wide
+    mul t1, a2, t2          # t1 = offset of idx'th elem
+    add t1, t1, a0          # t1 = &arr[idx]
+    lw t1, (t1)             # t1 = arr[idx]
+
+    mul t3, a3, t2          # offset of max_idx'th elem
+    add t3, t3, a0          # t3 = &add[max_idx]
+    lw t3, (t3)             # t3 = arr[max_idx]
+
+    bge t3, t1, not_max
+    mv t0, a2               # new_max = idx
+not_max:
+    addi a2, a2, 1          # idx + 1
+    mv a3, t0               # set up max_new
+    call fmi_rec_s
+    mv t0, a0               # ret val
+done:
+    mv a0, t0               # set up ret val
+    ld ra, (sp)             # restore ra
+    addi sp, sp, 8          # dealloc stack
     ret
