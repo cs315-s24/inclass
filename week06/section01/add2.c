280f03a751658a79fff71107ed90742af9fb88ae
Phil Peterson <phpeterson@usfca.edu>
2024-02-27 09:47:28 -0800

    add s01 2/27

---
+++ week06/section01/add2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int add2_c(int, int);
+int add2_s(int, int);
+
+
+void look_at_code(uint32_t *code) {
+    uint32_t add_instr = *code;
+    printf("add_instr: 0x%x\n", add_instr);
+
+    uint32_t ret_instr = *(code + 1);
+    printf("ret_instr: 0x%x\n", ret_instr);
+}
+
+int main(int argc, char **argv) {
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+    int sum = 0;
+
+    sum = add2_c(a, b);
+    printf("C: %d\n", sum);
+
+    sum = add2_s(a, b);
+    printf("Asm: %d\n", sum);
+
+    look_at_code((uint32_t*) add2_s);
+}
